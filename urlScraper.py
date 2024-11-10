b64d82ac69f0ed6065d50841db6c59b51b311d68
SpareABtye <104662326+SpareABtye@users.noreply.github.com>
2024-11-09 20:09:42 -0600

    Update urlScraper.py
    simplified url format function

--- urlScraper.py
+++ urlScraper.py
@@ -1,48 +1,36 @@
-import re
 import requests
 from bs4 import BeautifulSoup
 
-#Recieving the user_url_input from user.
-user_url_input = input('Please enter URL: ')
+#Recieving the url from user
+url = input('Please enter URL: ')
 
-#Function used to ensure https and www are placed in url to prevent bs4 errors.
-def verify_url(user_url_input):
-    http_head = 'https://'
-    www_head = 'www.'
-    fin_user_url_input = ''
-    
-    if www_head and http_head not in user_url_input:
-        if www_head not in user_url_input:
-            user_url_input = www_head + user_url_input
-        if http_head not in user_url_input:
-            user_url_input = http_head + user_url_input
-            
-    if www_head not in user_url_input:
-            parts = user_url_input.split('://')
-            parts[0] += '://'
-            parts.insert(1, 'www.')
-            user_url_input = ''.join(parts)
+#Function used to ensure https and www are place in url 
+def verify_url(url):
+    if '://' not in url:
+        url = 'https://' + url
         
-    return user_url_input
+    if not url.startswith('www.'):
+        url = url.replace('://', '://www.', 1)
+        
+    return url
 
-#Assigning a variable to call the above function and create a proper url.
-formated_url = verify_url(user_url_input)
+formated_url = verify_url(url)
 email_list = []
 
-#Trying to call url, verify a reachable status, parse html with bs4 and target emails in the page.
 try:
     response = requests.get(formated_url, timeout=10)
     response.raise_for_status()  # Raises an error for non-200 status codes
     soup = BeautifulSoup(response.text, 'html.parser')
-    emails = soup.find_all('a', class_='email')
+    #
     
-    #Adding to our email_list from the emails targets
+    emails = soup.find_all('a', class_='email')
     for i in emails:
         i_text = i.get_text()
         email_list.append(i_text)
     
+    #
+    #print(*email_list, sep='\n')
     print(f'URL: {formated_url}\nStatus: {response.status_code}\n \n' + '\n'.join(email_list))
-
 except requests.exceptions.RequestException as e:
     print(f'Error: Unable to reach the website: {formated_url}')
     print(e)
